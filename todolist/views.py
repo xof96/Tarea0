cad416e839d2bf68e5ed43fc9930d2e0b9efe1b8
Matias Pizarro <matias.pizarro.o96@gmail.com>
2018-09-18 14:42:03 -0300

    Task are capable to be deleted, put up and down

--- todolist/views.py
+++ todolist/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.urls import reverse
 
@@ -22,12 +22,57 @@ def add_task(request):
 
 
 def remove_task(request):
+    t_id = request.POST.get('erase', False)
+    if t_id is not False:
+        task_id = int(t_id)
+        try:
+            task = Task.objects.get(id=task_id)
+            task.delete()
+        except Task.DoesNotExist:
+            raise Http404("Tarea no existe")
+
     return HttpResponseRedirect(reverse('todolist:index'))
 
 
 def task_up(request):
+    t_id = request.POST.get('up', False)
+    if t_id is not False:
+        task_id = int(t_id)
+        try:
+            task = Task.objects.get(id=task_id)
+            try:
+                prev_id = task_id - 1
+                prev_task = Task.objects.get(id=prev_id)
+                task.id = prev_id
+                prev_task.id = task_id
+                task.save()
+                prev_task.save()
+            except (KeyError, Task.DoesNotExist):
+                raise render(request, 'todolist/index.html', {
+                    'error_message': "Tarea no puede ser subida"
+                })
+        except Task.DoesNotExist:
+            raise Http404("Tarea no existe")
     return HttpResponseRedirect(reverse('todolist:index'))
 
 
 def task_down(request):
+    t_id = request.POST.get('down', False)
+    if t_id is not False:
+        task_id = int(t_id)
+        try:
+            task = Task.objects.get(id=task_id)
+            try:
+                next_id = task_id + 1
+                next_task = Task.objects.get(id=next_id)
+                task.id = next_id
+                next_task.id = task_id
+                task.save()
+                next_task.save()
+            except (KeyError, Task.DoesNotExist):
+                raise render(request, 'todolist/index.html', {
+                    'error_message': "Tarea no puede ser bajada"
+                })
+        except Task.DoesNotExist:
+            raise Http404("Tarea no existe")
     return HttpResponseRedirect(reverse('todolist:index'))
